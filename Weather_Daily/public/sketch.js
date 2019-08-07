b8050a2632a25e256e0002dcbfdfcbe8dba8f317
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-08-07 14:25:42 -0300

    Add Air Quality Data on Weather Here

--- Weather_Daily/public/sketch.js
+++ Weather_Daily/public/sketch.js
@@ -14,13 +14,23 @@ function setup() {
             const response = await fetch(api_url);
             const data = await response.json();
             
+            const air = data.air_quality.results[0].measurements[0] || null;
+
             const temperatureCelcius = (data.weather.currently.temperature - 32) * 5/9;
 
+            // Weather DATA
             document.getElementById('latitude').textContent = lat.toFixed(2) ;
             document.getElementById('longitude').textContent = lon.toFixed(2);
             document.getElementById('summary').textContent = data.weather.currently.summary;
             document.getElementById('temperatureFahrenheit').textContent = data.weather.currently.temperature;
             document.getElementById('temperatureCelcius').textContent = temperatureCelcius.toFixed(1);  
+            
+            // AIR DATA
+            document.getElementById('aq_parameter').textContent = air.parameter;
+            document.getElementById('aq_value').textContent = air.value;
+            document.getElementById('aq_units').textContent = air.unit;
+            document.getElementById('aq_date').textContent = air.lastUpdated;
+        
         });
     }
     else {
