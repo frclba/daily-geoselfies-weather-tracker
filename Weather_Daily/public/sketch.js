ca9c8ded57fbee7b7bb18a1d2a54bb4a8d2ed0c3
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-08-08 16:09:53 -0300

    Mapping checkin entries with leaflet map

--- Weather_Daily/public/sketch.js
+++ Weather_Daily/public/sketch.js
@@ -14,6 +14,7 @@ function setup() {
             const response = await fetch(api_url);
             const data = await response.json();
             
+            const weather = data.weather.currently;
             const air = data.air_quality.results[0].measurements[0] || null;
 
             const temperatureCelcius = (data.weather.currently.temperature - 32) * 5/9;
@@ -21,8 +22,8 @@ function setup() {
             // Weather DATA
             document.getElementById('latitude').textContent = lat.toFixed(2) ;
             document.getElementById('longitude').textContent = lon.toFixed(2);
-            document.getElementById('summary').textContent = data.weather.currently.summary;
-            document.getElementById('temperatureFahrenheit').textContent = data.weather.currently.temperature;
+            document.getElementById('summary').textContent = weather.summary;
+            document.getElementById('temperatureFahrenheit').textContent = weather.temperature;
             document.getElementById('temperatureCelcius').textContent = temperatureCelcius.toFixed(1);  
             
             // AIR DATA
@@ -30,28 +31,21 @@ function setup() {
             document.getElementById('aq_value').textContent = air.value;
             document.getElementById('aq_units').textContent = air.unit;
             document.getElementById('aq_date').textContent = air.lastUpdated;
-        
+            
+            const db_data = {lat, lon, weather, air}
+
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(db_data)
+            };
+            const db_response = await fetch('/api', options);
+            const db_json = await db_response.json();
         });
     }
     else {
         console.log('geolocation not available');
     }
-
-    button.addEventListener('click', async event => {
-        const temperature = document.getElementById('temperature').value;
-        const data = { lat, lon, temperature};
-        
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify(data)
-        };
-
-        const response = await fetch('/api', options);
-        const json = await response.json();
-
-        console.log(json);
-    });        
 }
